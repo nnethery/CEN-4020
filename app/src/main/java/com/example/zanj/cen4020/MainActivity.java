b6d111ecc380b676c2d6dcaf5c25c2dee6c25244
Noah Nethery <nethery.n@gmail.com>
2017-10-17 16:20:52 -0400

    Added PubNub framework, changed permissions in manifest, and initialized PubNub in onCreate()

--- app/src/main/java/com/example/zanj/cen4020/MainActivity.java
+++ app/src/main/java/com/example/zanj/cen4020/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.zanj.cen4020;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Future reference, maybe add this to a main method so it loads
+        // and runs before app specific info is being set up
+        PNConfiguration pnConfiguration = new PNConfiguration();
+        pnConfiguration.setSubscribeKey("sub-c-ad43af60-b290-11e7-b4e4-2675c721e615");
+        pnConfiguration.setPublishKey("pub-c-a499a424-2f8a-4205-9c96-2492afd6349f");
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+
     }
 }
