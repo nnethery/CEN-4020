750ae8e6a97e0931fbcd056163fd9c4da7b124f4
CIS4930group <jcfriedman95@gmail.com>
2017-11-17 17:07:55 -0500

    Changed Date Format
    Dates are now printed in MM/dd/yyyy hh:mm format.

--- app/src/main/java/com/example/zanj/cen4020/DateTimeUtil.java
+++ app/src/main/java/com/example/zanj/cen4020/DateTimeUtil.java
@@ -3,12 +3,20 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.format.ISODateTimeFormat;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Convenience methods for getting timestamps.
  */
 public class DateTimeUtil {
     public static String getTimeStampUtc() {
-        return DateTime.now(DateTimeZone.UTC).toString(ISODateTimeFormat.dateTime());
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy hh:mm", Locale.US);
+        Date today = Calendar.getInstance().getTime();
+        return df.format(today);
     }
 
     public static String getTimeStampUtc(long instant) {
