65d9585bbb4adf00a14b73313f2c061fa3257be0
Alexis Taylor <alexismtaylor2@gmail.com>
2017-12-06 22:07:20 -0500

    Revert "Added left and right message bubbles for different users"
    This reverts commit 33d6d422795d0473d835e67e7db5ecb2063d5ed4.

--- app/src/main/java/com/example/zanj/cen4020/PubSubListRowUi.java
+++ app/src/main/java/com/example/zanj/cen4020/PubSubListRowUi.java
@@ -1,11 +1,8 @@
 package com.example.zanj.cen4020;
 
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class PubSubListRowUi { //row template for each message
-    public LinearLayout row_layout;
-    public LinearLayout msg_info_layout;
     public TextView sender;
     public TextView message;
     public TextView timestamp;
